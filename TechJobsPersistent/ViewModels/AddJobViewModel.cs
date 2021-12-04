75debd5f31ba0a923e24b9d502c3241c068bfb68
EducatedStrikeCart <dhn004@gmail.com>
2021-12-03 19:56:52 -0500

    add Job viewmodel

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public string Name { get; set; }
+        public string EmployerID { get; set; }
+        public List<Employer> SelectListItem { get; set; }
+    }
+}
